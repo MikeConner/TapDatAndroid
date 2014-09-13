b22df61f1ae4741883a5327a3d35795cb87e74c2
rasha <arash.danaie@gmail.com>
2014-09-13 17:54:39 -0400

    write tag updates - all account settings tab
    need to remove write activity

--- TapDatApp/app/src/main/java/co/tapdat/tapdatapp/AccountActivity.java
+++ TapDatApp/app/src/main/java/co/tapdat/tapdatapp/AccountActivity.java
@@ -4,7 +4,17 @@ import co.tapdat.tapdatapp.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,6 +22,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 
 /**
@@ -21,6 +34,11 @@ import android.support.v4.app.NavUtils;
  * @see SystemUiHider
  */
 public class AccountActivity extends Activity {
+
+    boolean mWriteMode = false;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mNfcPendingIntent;
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -191,8 +209,100 @@ public class AccountActivity extends Activity {
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
     public void startWrite (View view){
-        Intent i = new Intent(this, WriteActivity.class);
-        startActivity(i);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(AccountActivity.this);
+        mNfcPendingIntent = PendingIntent.getActivity(AccountActivity.this, 0,
+                new Intent(AccountActivity.this, AccountActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        enableTagWriteMode();
+
+        new AlertDialog.Builder(AccountActivity.this).setTitle("Touch tag to write")
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        disableTagWriteMode();
+                    }
+
+                }).create().show();
+
+
+      //  Intent i = new Intent(this, WriteActivity.class);
+      //  startActivity(i);
 
     }
+
+
+
+    private void enableTagWriteMode() {
+        mWriteMode = true;
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter[] mWriteTagFilters = new IntentFilter[] { tagDetected };
+        mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent, mWriteTagFilters, null);
+    }
+
+    private void disableTagWriteMode() {
+        mWriteMode = false;
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // Tag writing mode
+        if (mWriteMode && NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            // Get ID from local storage
+            // write ID to tag
+            String strMime = "tapdat/performer";
+            String strID = "TD482988DS";
+            NdefRecord record = NdefRecord.createMime( strMime, strID.getBytes());
+            NdefMessage message = new NdefMessage(new NdefRecord[] { record });
+            if (writeDaTag(message, detectedTag)) {
+                Toast.makeText(this, "Success: Wrote placeid to nfc tag", Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    }
+
+
+    /*
+    * Writes an NdefMessage to a NFC tag
+    */
+    public boolean writeDaTag(NdefMessage message, Tag tag) {
+        int size = message.toByteArray().length;
+        try {
+            Ndef ndef = Ndef.get(tag);
+            if (ndef != null) {
+                ndef.connect();
+                if (!ndef.isWritable()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error: tag not writable",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                if (ndef.getMaxSize() < size) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error: tag too small",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                ndef.writeNdefMessage(message);
+                return true;
+            } else {
+                NdefFormatable format = NdefFormatable.get(tag);
+                if (format != null) {
+                    try {
+                        format.connect();
+                        format.format(message);
+                        return true;
+                    } catch (IOException e) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
