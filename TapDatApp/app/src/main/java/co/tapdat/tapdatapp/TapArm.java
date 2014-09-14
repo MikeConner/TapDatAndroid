046e3435fd455df2a342946d9bf3a07bf47fabdc
arash <arash.danaie@gmail.com>
2014-09-14 19:02:11 -0400

    Tap from anywhere / send mode
    more

--- TapDatApp/app/src/main/java/co/tapdat/tapdatapp/TapArm.java
+++ TapDatApp/app/src/main/java/co/tapdat/tapdatapp/TapArm.java
@@ -13,6 +13,16 @@ import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 import android.content.Intent;
 import android.widget.TextView;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.NdefRecord;
+import android.nfc.tech.Ndef;
+import android.nfc.NdefMessage;
+import android.os.AsyncTask;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import android.util.Log;
+import android.widget.Button;
 
 import co.tapdat.tapdatapp.R;
 
@@ -67,9 +77,23 @@ public class TapArm extends Activity {
         final View contentView = findViewById(R.id.fullscreen_content);
 
         Intent i = getIntent();
-        float fltDefault = 1;
-        strTipMessage  = Float.toString( i.getFloatExtra("TIPAMOUNT",fltDefault)) ;
-        strTagID = i.getStringExtra("TAGID");
+
+
+        //figure out if this came in from NFC tag OR from Armed area
+
+        String action = i.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            strTipMessage = "Ready to Tip ";
+            Tag tag = i.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            new NdefReaderTask().execute(tag);
+        }
+        else{
+            float fltDefault = 1;
+            strTipMessage  = Float.toString( i.getFloatExtra("TIPAMOUNT",fltDefault)) ;
+            strTagID = i.getStringExtra("TAGID");
+            strTipMessage = "You Just TIPPED " + strTagID + " $"+ strTipMessage + "!";
+        }
+
 
         // Set up an instance of SystemUiHider to control the system UI for
         // this activity.
@@ -126,7 +150,7 @@ public class TapArm extends Activity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         TextView txtAfterTap = (TextView) findViewById(R.id.txtAfterTap);
-        txtAfterTap.setText("You Just TIPPED " + strTipMessage + ".  Much appreciated by: " + strTagID);
+        txtAfterTap.setText(strTipMessage);
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
         // are available.
@@ -236,9 +260,76 @@ public class TapArm extends Activity {
 
     }
 
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
 
+        @Override
+        protected String doInBackground(Tag... params) {
+            Tag tag = params[0];
+
+            Ndef ndef = Ndef.get(tag);
+            if (ndef == null) {
+                // NDEF is not supported by this Tag.
+                return null;
+            }
+
+            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+            NdefRecord[] records = ndefMessage.getRecords();
+            for (NdefRecord ndefRecord : records) {
+                //if ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)
+                if (true) {
+                    try {
+                        return readText(ndefRecord);
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e("tag", "Unsupported Encoding", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+            byte[] payload = record.getPayload();
+
+            // Get the Text Encoding
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+
+            // Get the Language Code
+            int languageCodeLength = payload[0] & 0063;
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                TextView txtAfterTap = (TextView) findViewById(R.id.txtAfterTap);
+                txtAfterTap.setText("Ready to Tip: " + result);
+                Button btnTapDat = (Button) findViewById(R.id.btnTapDat);
+                btnTapDat.setText("SEND");
+                //put in code to change teh mode to send vs. tap -> no going to armed!!!
+            }
+        }
+    }
 
 
 
 
 }
+
+
